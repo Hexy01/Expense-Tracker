e84c5191331f410bc3a53caf201595199c7c1651
Dhanya <149366517+Hexy01@users.noreply.github.com>
2025-06-24 02:27:40 +0530

    Delete models directory

--- models/item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  status: { type: String, enum: ["available", "out of stock"], default: "available" },
-});
-
-module.exports = mongoose.model("Item", ItemSchema);
